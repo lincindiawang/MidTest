4a3d4aa2bd10a21acb41c42fd843df0ba4862d81
lincindiawang <lindawang0207@gmail.com>
2019-12-03 15:47:05 +0800

    1203 2nd

--- app/src/main/java/com/fju/water/MainActivity.java
+++ app/src/main/java/com/fju/water/MainActivity.java
@@ -15,9 +15,11 @@ import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.Switch;
 import android.widget.TextView;
 
@@ -118,6 +120,19 @@ public class MainActivity extends AppCompatActivity {
                 isNext = isChecked;
                 TextView text = findViewById(R.id.type);
                 text.setText(isNext ? getString(R.string.every_other_month) : getString(R.string.monthly));
+
+            }
+        });
+        //spinner
+        Spinner cities = findViewById(R.id.spinner);
+        cities.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Log.d(TAG, getResources().getStringArray(R.array.cities)[position]);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
 
             }
         });
